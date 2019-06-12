7e0c724b4cca7df7e8acc6fedd1bd35dc03b41e4
Jovan Dandridge <quantumcoder01@gmail.com>
2019-06-11 18:59:26 -0600

    deleted/added files

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -68,7 +68,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'debug': DEBUG,
         },
     },
 ]
